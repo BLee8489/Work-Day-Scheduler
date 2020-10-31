86dc5d30c063a138b2837308219d7c1439a390f8
“bonnielee” <bonlee8489@gmail.com>
2020-10-31 16:28:36 -0700

    final edits to JS

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $("span").attr("style", "width: 75px")
     $("button").text("Submit")
 
-    const times = [9, 10, 11, 12, 13, 14, 15, 16, ]
+    const times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
     times.forEach(time => {
         const timeCheck = window.localStorage.getItem(time)
